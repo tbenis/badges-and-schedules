3d2b4304f237f342cbfeff2b01e89e0c3244b5ca
Benis Tambe <will.jenifer@yahoo.com>
2021-01-15 17:35:00 -0500

    commit tree 420e1d8937a125a30c73fc33d4729cca5a8a5105

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,41 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}." 
+end
+
+def batch_badge_creator(name_array)
+    newArr = []
+    
+    name_array.each do |name1|
+        newArr << "Hello, my name is #{name1}." 
+    end
+    return newArr
+end
+
+
+def assign_rooms(name_array)
+    newArr1 = []
+    room = 1
+    
+    name_array.each do |name2|
+        newArr1 << "Hello, #{name2}! You'll be assigned to room #{room}!"
+        room +=1
+    end
+    
+    return newArr1
+end
+
+
+
+def printer(speakers)
+
+    batch_badge_creator(speakers).each do |name3|
+
+        puts name3 
+    end
+    assign_rooms(speakers).each do |room|
+        puts room 
+    end
+
+end
